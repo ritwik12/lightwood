68fea46b9bdc13dfb2b93124c05d649dbcac3b93
George3d6 <george@cerebralab.com>
2019-09-15 05:23:14 +0000

    fixed some naming issues, added pytorhc wrapper

--- lightwood/encoders/text/bert.py
+++ lightwood/encoders/text/bert.py
@@ -1,27 +1,27 @@
+import torch
 from pytorch_transformers import DistilBertModel, DistilBertTokenizer
 
 
 class BertEncoder:
     def __init__(self):
-        self.tokenizer = DistilBertModel.from_pretrained('distilbert-base-uncased')
-        self.model = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        print(self.model)
-        exit()
+        self._tokenizer = DistilBertModel.from_pretrained('distilbert-base-uncased')
+        self._model = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+        self._pytorch_wrapper = torch.FloatTensor
 
     def encode(self, column_data):
 
         tokenized_inputs = []
         for text in column_data:
-            tokenized_input = torch.tensor([tokenizer.encode(text, add_special_tokens=True)])
+            tokenized_input = torch.tensor([self._tokenizer.encode(text, add_special_tokens=True)])
             tokenized_inputs.append(tokenized_input)
 
         encoded_representation = []
         with torch.no_grad():
             for tokenized_input in tokenized_inputs:
-                last_hidden_states = model(tokenized_input)[0]
-                print(last_hidden_states)
+                last_hidden_states = self._model(tokenized_input)[0]
                 encoded_representation.append(last_hidden_states)
-
+                print(last_hidden_states)
+                exit()
         return self._pytorch_wrapper(encoded_representation)
 
 
