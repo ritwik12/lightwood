fa012948ec4fede15d2892dcb223b32ea1498eef
George3d6 <george@cerebralab.com>
2019-09-16 04:42:23 +0000

    using normalized embedings

--- lightwood/encoders/text/bert.py
+++ lightwood/encoders/text/bert.py
@@ -9,20 +9,37 @@ class BertEncoder:
         self._pytorch_wrapper = torch.FloatTensor
 
     def encode(self, column_data):
+        encoded_representation = []
 
         tokenized_inputs = []
         for text in column_data:
             tokenized_input = torch.tensor([self._tokenizer.encode(text, add_special_tokens=True)])
+
+            encoded_representation.append(list(tokenized_input))
+
+            tokenized_input = torch.tensor(list(tokenized_input[0][0:512]))
             tokenized_inputs.append(tokenized_input)
 
-        encoded_representation = []
+        max_len = max([len(x) for x in encoded_representation])
+        max_element = 0
+        for arr in encoded_representation:
+            for ele in arr:
+                if ele > max_element:
+                    max_element = ele
+
+        for arr in encoded_representation:
+            if len(arr) < max_len:
+                arr.append(0)
+            for i in range(len(arr)):
+                arr[i] = arr[i]/max_element
+
+        return self._pytorch_wrapper(encoded_representation)
+
+        # See this later
         with torch.no_grad():
             for tokenized_input in tokenized_inputs:
                 last_hidden_states = self._model(tokenized_input)[0]
                 encoded_representation.append(last_hidden_states)
-                print(last_hidden_states)
-                exit()
-        return self._pytorch_wrapper(encoded_representation)
 
 
     def decode(self, encoded_values_tensor, max_length = 100):
