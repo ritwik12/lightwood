8da24b32e7b0f6d926ede1a7dca73ca447b81c9b
George3d6 <george@cerebralab.com>
2019-09-17 04:00:38 +0000

    tensor sizing correction

--- lightwood/encoders/text/bert.py
+++ lightwood/encoders/text/bert.py
@@ -12,16 +12,17 @@ class BertEncoder:
 
     def encode(self, column_data):
         encoded_representation = []
-        print(self._max_len)
         tokenized_inputs = []
         for text in column_data:
             tokenized_input = torch.tensor([self._tokenizer.encode(text, add_special_tokens=True)])
 
-            encoded_representation.append(list(tokenized_input[0]))
-
+            #encoded_representation.append(list(tokenized_input[0]))
             tokenized_input = torch.tensor(list(tokenized_input[0][0:512]))
+            tokenized_input.unsqueeze_(-1)
+            tokenized_input = tokenized_input.transpose(1,0)
             tokenized_inputs.append(tokenized_input)
 
+        '''
         if self._max_len is None:
             self._max_len = max([len(x) for x in encoded_representation])
 
@@ -42,13 +43,17 @@ class BertEncoder:
                 arr[i] = arr[i]/self._max_ele
 
         return self._pytorch_wrapper(encoded_representation)
-
+        '''
         # See this later
         with torch.no_grad():
             for tokenized_input in tokenized_inputs:
-                last_hidden_states = self._model(tokenized_input)[0]
+                hidden_states = self._model(tokenized_input)
+                print(hidden_states.size())
+                print(hidden_states[0].size())
+                print(hidden_states[0][0].size())
                 encoded_representation.append(last_hidden_states)
 
+        return self._pytorch_wrapper(encoded_representation)
 
     def decode(self, encoded_values_tensor, max_length = 100):
         # When test is an output... a bit trickier to handle this case, thinking on it
